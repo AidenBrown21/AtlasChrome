a5c5a46187ac57ef2693c864043dfb53b0abce50
Rahul Menon <rahul.menon85280@gmail.com>
2025-06-06 23:45:33 -0500

    All updates so far, lets hope we can deploy with this

--- backend/app.py
+++ backend/app.py
@@ -122,4 +122,4 @@ def image_analyze():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
