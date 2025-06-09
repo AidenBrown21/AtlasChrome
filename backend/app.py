5285c3fa19a733bafc9f26d01cd9e53fd6a0ec77
Rahul Menon <rahul.menon85280@gmail.com>
2025-06-08 22:54:12 -0500

    host update on backend

--- backend/app.py
+++ backend/app.py
@@ -122,4 +122,4 @@ def image_analyze():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)), host="0.0.0.0")
