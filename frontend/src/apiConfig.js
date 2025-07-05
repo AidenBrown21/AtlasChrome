2d5784ba7912850fd82e550d1fd93d95205f9c0a
Rahul Menon <rahul.menon85280@gmail.com>
2025-07-05 16:01:02 -0500

    Backend fic

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -2,6 +2,6 @@
 //   ? process.env.REACT_APP_API_URL
 //   : 'https://atlas-wmhn.onrender.com';
 
-const REACT_APP_API_URL="atlas-backend-fkgye9e7b6dkf4cj.westus-01.azurewebsites.net"
+const REACT_APP_API_URL="https://atlas-backend-fkgye9e7b6dkf4cj.westus-01.azurewebsites.net"
 // export default API_URL; 
 export default REACT_APP_API_URL;
